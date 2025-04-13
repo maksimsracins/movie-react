448424ba36b0c4d06066a25c13369e1b096f35e7
maksimsracins <56410320+maksimsracins@users.noreply.github.com>
2025-04-13 15:18:51 +0300

    api added

---
+++ src/services/api.js
@@ -0,0 +1,9 @@
+const API_KEY = "88adb9df3908f30f89af8ae5a2e273ca"
+const BASE_URL = "https://api.themoviedb.org/3"
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json();
+    return data.results
+};
+
